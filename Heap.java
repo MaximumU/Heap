82d2a154b5c759396c2789c9a40c45b0208dcbc0
MaximumU <theliamwalker4@gmail.com>
2026-02-18 17:45:11 +0000

    finished

--- Heap.java
+++ Heap.java
@@ -2,6 +2,7 @@ public class Heap {
 
 //the actual storage structure for your heap
 private int[] arr;
+private int size = 0;
 
 //constructor for your heap
 //feel free to make one that takes in an array if you prefer for your testing purposes.
@@ -9,7 +10,6 @@ private int[] arr;
 //the array
 public Heap() {
     arr = new int[100];
-    int size = 0;
 }
 
 //create this function to add elements to your heap
@@ -26,7 +26,7 @@ public void add(int toAdd) {
 //5 points
 public void removeMax() {
     int curr = 0;
-    size --:
+    size --;
     arr[curr] = arr[size];
     arr[size] = 0;
     siftDown(curr);
@@ -35,18 +35,18 @@ public void removeMax() {
 //this should check and alter the tree after an item is inserted
 //3 points
 private void siftUp(int toAdd, int curr){
-    if(arr[curr] > arr((curr-1)/2)){
+    if(arr[curr] > arr[(curr-1)/2]){
         int temp = arr[(curr-1)/2];
         arr[(curr-1)/2] = arr[curr];
         arr[curr] = temp;
-        add(toAdd,(curr-1)/2);
+        siftUp(toAdd,(curr-1)/2);
     }
 }
 
 //this should check and alter the tree after an item is deleted.
 //3 points
 private void siftDown(int curr) {
-    if (arr[curr]<arr[(2*curr)+2 && arr[(2*curr)+2]>arr[(2*curr)+1]]){
+    if (arr[curr] < arr[(2*curr)+2] && arr[(2*curr)+2]>arr[(2*curr)+1]){
         if(arr[(2*curr)+2] > arr[curr]){
             int temp = arr[(2*curr)+2];
             arr[(2*curr)+2] = arr[curr];
@@ -54,7 +54,7 @@ private void siftDown(int curr) {
             siftDown((2*curr)+2);
         }
     }
-    else if(arr[curr]<arr[(2*curr)+2 && arr[(2*curr)+2]>arr[(2*curr)+1]]){
+    else if(arr[curr]<arr[(2*curr)+2] && arr[(2*curr)+2]>arr[(2*curr)+1]){
         if(arr[(2*curr)+1] > arr[curr]){
             int temp = arr[(2*curr)+1];
             arr[(2*curr)+1] = arr[curr];
@@ -65,7 +65,7 @@ private void siftDown(int curr) {
     return;
 }
 
-Public static void main(String[] args) {
+public static void main(String[] args) {
     Heap h = new Heap();
     h.add(5);
     h.add(10);
